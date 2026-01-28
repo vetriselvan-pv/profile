aef7a69d489e6db73e6c8d9fb45009ddc7a1c211
Vetriselvan Panneerselvam <85449760+vetriselvan-pv@users.noreply.github.com>
2026-01-28 05:48:32 +0530

    app config changes

--- server/src/server.ts
+++ server/src/server.ts
@@ -47,10 +47,10 @@ app.post(
   },
 );
 
-const PORT = process.env.PORT || 3000;
-if (process.env.NODE_ENV !== 'production') {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
+// const PORT = process.env.PORT || 3000;
+// if (process.env.NODE_ENV !== 'production') {
+//   app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`);
+//   });
+// }
  export default app;
