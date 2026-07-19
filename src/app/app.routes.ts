f854f7ad05da1b2c8f4551d241d670d667b32116
vetriselvan-pv <learnerkid11@gmail.com>
2026-07-19 22:38:57 +0530

    fresh branch

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { MainContent } from './main-content';
-import { BlogsPage } from './blog/blogs-page';
-
-export const routes: Routes = [
-  { path: '', component: MainContent },
-  { path: 'blogs', component: BlogsPage },
-  { path: '**', redirectTo: '' }
-];
--- src/app/pipes/safesvg/safesvg-pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SafesvgPipe } from './safesvg-pipe';
-
-describe('SafesvgPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SafesvgPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
